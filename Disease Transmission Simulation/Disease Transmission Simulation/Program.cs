e48dd7fa6f4ced0c47c67f11aa52665d42f590d0
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-02-28 11:48:51 +0700

    Initial project commit

---
+++ Disease Transmission Simulation/Disease Transmission Simulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Disease_Transmission_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
