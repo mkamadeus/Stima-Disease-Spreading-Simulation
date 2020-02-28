a1c33037e50534d2ee322b68d227e4e9caa14bdf
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-02-29 01:57:12 +0700

    Filereader input added, not yet converted to graph ds.

--- Disease Transmission Simulation/Program.cs
+++ Disease Transmission Simulation/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using static Disease_Transmission_Simulation.FileReader;
 
 namespace Disease_Transmission_Simulation
 {
@@ -9,9 +7,10 @@ namespace Disease_Transmission_Simulation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            FileReader f = new FileReader();
+            FileReader f = new FileReader("D:\\run.txt", "D:\\run2.txt");
 
             f.printAllLines();
+            f.convertToGraphData();
         }
     }
 }
