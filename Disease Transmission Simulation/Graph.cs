7bed08255f94bc40df3a4cc09aa6e20d0ba3943b
stefanusgusega <gunawanstefanus006@gmail.com>
2020-03-01 13:38:48 +0700

    kelas graf

---
+++ Disease Transmission Simulation/Graph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Disease_Transmission_Simulation
+{
+	public class Graf
+	{
+		private int[,] matrix;
+		private double[] Tr;
+		public Graf()
+		{
+			this.matrix = new int[26, 26];
+			this.Tr = new double[26 * 26];
+			for (int i = 0; i < 26; i++)
+			{
+				for (int j = 0; j < 26; j++)
+				{
+					this.matrix[i, j] = 0;
+					this.Tr[26 * i + j] = 0;
+				}
+			}
+		}
+		public int GetKetetanggaan(int i, int j) { return this.matrix[i, j]; }
+		public double GetTr(int i, int j) { return this.Tr[26 * i + j]; }
+		public void Set(int i, int j, int baru, double Tr)
+		{
+			this.matrix[i, j] = baru;
+			this.Tr[26 * i + j] = Tr;
+		}
+		public void addEdge(int i, int j, double Tr)
+		{
+			if (GetKetetanggaan(i,j)==0)
+			{
+				Set(i, j, 1,Tr);
+			}
+			else
+			{
+				Console.WriteLine("Sudah ada.");
+			}
+		}
+		public void printGraf()
+		{
+			for (int i = 0; i<26; i++)
+			{
+				for (int j = 0; j < 26; j++)
+				{
+					if (GetKetetanggaan(i,j)==1)
+					{
+						Console.WriteLine(i + " " + j + " " + this.Tr[26 * i + j]);
+					}
+				}
+			}
+		}
+	}
+}
--- Disease Transmission Simulation/Program.cs
+++ Disease Transmission Simulation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 using static Disease_Transmission_Simulation.FileReader;
 
@@ -8,10 +10,21 @@ namespace Disease_Transmission_Simulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            FileReader f = new FileReader();
+            //Console.WriteLine("Hello World!");
+            //FileReader f = new FileReader();
 
-            f.printAllLines();
+            //f.printAllLines();
+            Graf G = new Graf();
+            G.addEdge(0, 1, 0.02);
+            G.addEdge(0, 2, 0.005);
+            G.addEdge(1, 0, 0.005);
+            G.addEdge(1, 3, 0.005);
+            G.addEdge(2, 0, 0.1);
+            G.addEdge(2, 1, 0.1);
+            G.addEdge(3, 0, 0.05);
+            G.addEdge(3, 2, 0.1);
+            G.printGraf();
+
         }
     }
 }
